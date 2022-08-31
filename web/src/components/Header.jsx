a5ebe81573e5397c7af57094069f2b8b043eef6e
nikitkhakholia <khakholia.nk@gmail.com>
2022-08-31 18:04:58 +0530

    nav bar

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -1,27 +1,72 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const logo = require("../assets/logo.png");
+import "./header.css";
 export default function Header() {
+  let navigate = useNavigate();
   return (
-    <div className="bg-black text-white">
-      <div className=" flex ">
-        <div className="basis-full md:basis-1/3">11</div>
-        <div className="basis-full md:basis-2/3">22</div>
+    <header className="tw-text-gray-800">
+      <ul className="tw-flex tw-justify-between tw-items-center">
+        <li className="tw-mx-4 tw-my-2">
+          <div
+            class="hamburger"
+            id="hamburger-1"
+            onClick={(e) => {
+              document
+                .getElementById("hamburger-1")
+                .classList.toggle("is-active");
+            }}
+          >
+            <span class="line"></span>
+            <span class="line"></span>
+            <span class="line"></span>
+          </div>
+        </li>
+        <li
+          className="tw-mx-4 subpixel-antialiased tw-font-bold tw-text-4xl tw-tracking-wide tw-cursor-pointer tw-transition
+                        hover:tw-scale-110
+        "
+          onClick={(e) => {
+            navigate("/");
+          }}
+        >
+          RàsBérry
+        </li>
+        <li className="tw-mx-4 tw-cursor-pointer">Login</li>
+      </ul>
+      <div
+        className="tw-backdrop-blur-md tw-absolute tw-inset-0 tw-flex tw-justify-center tw-items-center"
+        id="login-modal"
+      >
+        <div className="tw-bg-slate-50 tw-p-4 tw-max-w-sm tw-rounded tw-shadow-md tw-text-gray-800">
+          {/* modal header */}
+          <div className="tw-flex tw-justify-between tw-items-center tw-mb-3">
+            <h4 className="tw-font-semibold tw-text-xl">Welcome to RàsBérry</h4>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="tw-w-6 tw-h-6 tw-cursor-pointer"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </div>
+          {/* modal body */}
+          <div className="">
+            <p>Please enter your details,</p>
+            <div className="tw-w-100 tw-m-1 tw-cursor-pointer tw-text-center tw-p-2 tw-bg-white tw-rounded">Login with Google</div>
+            <p className="tw-text-center">-or-</p>
+            <label>Email</label>
+            <input type="text"/>
+          </div>
+        </div>
       </div>
-      {/* <div class="flex flex-row">
-        <div class="basis-1/4 md:basis-auto">01</div>
-        <div class="basis-1/4 md:basis-auto">02</div>
-        <div class="basis-1/2 md:basis-auto">03</div>
-      </div> */}
-    </div>
-    //       {/* <img className="w-24" src={logo} alt="" />
-    //   <h1 className=" text-5xl">Rasvery </h1> */}
-    //      {/* <div className="flex flex-reverse">
-    //     <div class="">
-    //       <input type="text" placeholder="Search Anything" />
-    //     </div>
-    //     <div class=" bg-blue-900">Cart</div>
-    //     <div class=" bg-green-900 ">Menu</div>
-    //   </div> */}
+    </header>
   );
 }
--- web/src/index.jsx
+++ web/src/index.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
 import "./index.css";
-import Home from "./pages/Home";
+import { BrowserRouter, Routes } from "react-router-dom";
+import Header from "./components/Header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <Routes></Routes>
   </BrowserRouter>
 );
